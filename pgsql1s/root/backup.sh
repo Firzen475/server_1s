252cbc98e8c087e89f5af0c57e9a78cf804763b3
Firzen475 <firzen475@gmail.com>
2023-01-28 20:48:12 +0300

    Update backup.sh

--- pgsql1s/root/backup.sh
+++ pgsql1s/root/backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mydate=$(date '+%Y_%m_%d_%H_%M_%S')
-/opt/pgpro/1c-15/bin/pg_dumpall -U postgres > /backup/full_dump_$mydate.sql 2> /var/log/pg_dumpall.log
+/opt/pgpro/1c-15/bin/pg_dumpall -U postgres -c | gzip > /backup/full_dump_$mydate.gz 2> /var/log/LOG_pgsql.log
 find /backup/ -mindepth 1 -mtime +1100 -delete
