f17f76af784bb5b7d1481e5fd7ed20a11ce9bd15
Firzen475 <firzen475@gmail.com>
2023-01-29 03:24:41 +0300

    Update init.sh

--- pgsql1s/init.sh
+++ pgsql1s/init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-mkdir -p /_1s/backup
-mkdir -p /_1s/bd
-mv ./root/ /_1s/
+[ -d /_1s/backup/ ] || mkdir -p /_1s/backup
+[ -d /_1s/bd/ ] || mkdir -p /_1s/bd
+[ -d /_1s/root/ ] || mv ./root/ /_1s/
 
-touch ./.env
-echo 'TZ="Asia/Yekaterinburg"     # Europe/Moscow Asia/Yekaterinburg' >> ./.env
-echo 'PASS="password"        # Пароль пользователя postgres' >> ./.env
-echo 'SHEDULE="0 0 * * *"  # Расписание в формате cron * * * * *' >> ./.env
+[ -f ./.env ] || touch ./.env && \
+  echo 'TZ="Asia/Yekaterinburg"     # Europe/Moscow Asia/Yekaterinburg' >> ./.env && \
+  echo 'PASS="password"        # Пароль пользователя postgres' >> ./.env && \
+  echo 'SHEDULE="0 0 * * *"  # Расписание в формате cron * * * * *' >> ./.env && \
