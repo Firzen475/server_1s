3235b85376760fce7175326d60960a6a2d107929
Firzen475 <firzen475@gmail.com>
2023-02-01 15:25:21 +0300

    Update entrypoint.sh

--- pgsql1s/entrypoint.sh
+++ pgsql1s/entrypoint.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
-chmod +x /root/run.sh
-echo "TZ: ${TZ}"
-echo "PASS: ${PASS}"
-echo "SHEDULE: ${SHEDULE}"
-if [ "$(ls -A /var/lib/pgpro/1c-15/data)" ]
+chown postgres:postgres /database/
+if [ "$(ls -A /database/)" ]
 then
         echo "DATABASE EXISTS!"
 else
         echo "DATABASE EMPTY, INIT...!"
-        /opt/pgpro/1c-15/bin/pg-setup initdb
+        gosu postgres /opt/pgpro/1c-15/bin/initdb --locale=ru_RU.UTF-8 -D "/database/"
+        sed -i 's/local   all             all                                     peer/local   all   postgres   trust/g' /database/pg_hba.conf
+        echo "host    all         all         172.16.238.0/24    md5" >> /database/pg_hba.conf
 fi
+
 echo postgres:${PASS} | chpasswd
-sed -i 's/local   all             all                                     peer/local   all   postgres   trust/g' /var/lib/pgpro/1c-15/data/pg_hba.conf
-/opt/pgpro/1c-15/bin/pg-setup service start
+gosu postgres /opt/pgpro/1c-15/bin/pg_ctl -D "/database/" --log "/database/strat.log" -o "-c listen_addresses='*'" -w start
 /opt/pgpro/1c-15/bin/psql -U postgres -c "ALTER USER postgres WITH PASSWORD '${PASS}';"
+echo "//////////////////"
+chmod +x /root/run.sh
+chmod +x /root/healthcheck.sh
+echo "PASS: ${PASS}"
+crontab -l
 cron -f
+
+
+
+
 
 
 
