961b3c4fef6ac2ee4f48a005ec013be7407f2dea
Firzen475 <firzen475@gmail.com>
2023-02-02 18:46:43 +0300

    Update init.sh

--- srv1s/init.sh
+++ srv1s/init.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 [ -d /_1s/srvConfig/ ] || mkdir -p /_1s/srvConfig
-[ -d /_1s/root_srv1s/ ] || mv ./srv1s/root_srv1s /_1s/root_srv1s
-[ -d /_1s/apacheConf/ ] || mv ./srv1s/apacheConf /_1s/apacheConf
-[ -d /_1s/apacheDir/ ] || mv ./srv1s/apacheDir /_1s/apacheDir
+[ -d /_1s/root_srv1s/ ] || mv ./root_srv1s /_1s/root_srv1s
+[ -d /_1s/apacheConf/ ] || mv ./apacheConf /_1s/apacheConf
+[ -d /_1s/apacheDir/ ] || mv ./apacheDir /_1s/apacheDir
 
 openssl req -x509 -nodes -days 4000 -newkey rsa:2048 -keyout /_1s/root_srv1s/srv1s.key -out /_1s/root_srv1s/srv1s.crt -config ./srv1s.conf
+
+if [[ ! -f ./.env ]]; then
+  touch ./.env && \
+  echo 'TZ="Europe/Moscow"     # Europe/Moscow Asia/Yekaterinburg' >> ./.env
+fi
+
