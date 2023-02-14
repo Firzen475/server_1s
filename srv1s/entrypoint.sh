86044154d05f1f2ea3888cc8082fe706b75549f0
Firzen475 <firzen475@gmail.com>
2023-02-14 20:22:17 +0500

    Update entrypoint.sh

--- srv1s/entrypoint.sh
+++ srv1s/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 if [ -f "/root/srv1s.crt" ] && [ -f "/root/srv1s.key" ] ; then
         echo "ServerName $(cat /etc/hostname)" >> /etc/apache2/apache2.conf
         for f in /var/www/1c/*; do
@@ -16,19 +17,13 @@ if [ -f "/root/srv1s.crt" ] && [ -f "/root/srv1s.key" ] ; then
         /etc/init.d/apache2 restart
 fi
 echo "//////////////////"
-export SRV1CV8_KEYTAB=/root/usr1cv8.keytab
-#gosu usr1cv8 $(find /opt/1cv8/x86_64/ -name ragent) \
-$(find /opt/1cv8/x86_64/ -name ragent) \
-                        -d /srvConfig/ \
-                        -port 1540 \
-                        -regport 1541 \
-                        -range 1560:1591 \
-                        -seclev 0 \
-                        -pingPeriod 1000 \
-                        -pingTimeout 5000 \
-                        -debug
-
-
+chown -R usr1cv8:grp1cv8 /opt/1cv8/*
+chown -R usr1cv8:grp1cv8 /srvConfig/*
+chown -R usr1cv8:grp1cv8 /srvConfig/
+chown -R usr1cv8:grp1cv8 /home/usr1cv8/
+chown -R usr1cv8:www-data /root/usr1cv8.keytab
+chmod 660 /root/usr1cv8.keytab
+su -s /bin/sh  - "usr1cv8" -c "export Environment=SRV1CV8_KEYTAB=/root/usr1cv8.keytab; export SRV1CV8_KEYTAB=/root/usr1cv8.keytab; $(find /opt/1cv8/x86_64/ -name ragent) -d /srvConfig/ -port 1540 -regport 1541 -range 1560:1591 -seclev 0 -pingPeriod 1000 -pingTimeout 5000"
 
 
 
